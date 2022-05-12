fc446ac540a44c4d3ecf06f1ae18d3cd38e4d58c
David Murdoch <david@davidmurdoch.com>
2022-05-12 09:29:59 -0400

    more

--- next.config.js
+++ next.config.js
@@ -1,6 +1,9 @@
+const debug = process.env.NODE_ENV !== "production";
+
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  reactStrictMode: true
+  reactStrictMode: true,
+  assetPrefix: !debug ? 'https://trufflesuite.github.io/clairvoyance/' : '',
 }
 
 module.exports = nextConfig
