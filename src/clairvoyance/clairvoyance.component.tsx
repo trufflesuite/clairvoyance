88ea20ca37127cc177a32374ee542951a5caedb4
David Murdoch <david@davidmurdoch.com>
2022-05-12 21:57:53 -0400

    pretty tabs

--- src/clairvoyance/clairvoyance.component.tsx
+++ src/clairvoyance/clairvoyance.component.tsx
@@ -1,5 +1,5 @@
+import * as Chakra from "@chakra-ui/react";
 import PropTypes from 'prop-types';
-import { useState } from 'react';
 import { TransactionDecoding } from "../decoding";
 import { TransactionSimulation } from '../transaction-simulation/transaction-simulation.component';
 import { TransactionDetails} from "../transaction-details/transaction-details.component"
@@ -9,33 +9,33 @@ import { useDecoder } from './hooks/useDecoder';
 
 
 export function Clairvoyance({ options }: {options: Options}){
-  const [tab, setTab] = useState<"simulation" | "debug">("simulation");
   const {provider} = useGanache(options.options);
   const decoderOptions = {provider, addresses: [options.to], compilations: [], networkId: options.networkId};
   const {decoder, compilations} = useDecoder(decoderOptions);
 
-  if(!provider){
+  if (!provider) {
     return <div>Loading...</div>
   }
 
   return (
-    <div className="tx-insight">
+    <Chakra.Box>
       <TransactionDetails options={options.options} from={options.from} tx={options.tx}/>
       <TransactionDecoding decoder={decoder} provider={provider} options={options.options} from={options.from} tx={options.tx} networkId={options.networkId} />
-      <div className="tabs">
-        <div className="tab-container>">
-          <button className={tab === "simulation" ? "tab-active" : ""} onClick={()=>setTab(
-            "simulation"
-          )}>Simulation</button>
-          <button className={tab === "debug" ? "tab-active" : ""} onClick={()=>setTab(
-            "debug"
-          )}>Debug</button>
-        </div>
-        <div className="tabContents">
-          { tab === "simulation" ? <TransactionSimulation networkId={options.networkId} from={options.from} tx={options.tx} compilations={compilations} provider={provider} /> : <div>todo</div> }
-        </div>
-      </div>
-    </div>
+      <Chakra.Tabs>
+        <Chakra.TabList>
+          <Chakra.Tab>Simulation</Chakra.Tab>
+          <Chakra.Tab>Debugger</Chakra.Tab>
+        </Chakra.TabList>
+        <Chakra.TabPanels>
+          <Chakra.TabPanel>
+            <TransactionSimulation networkId={options.networkId} from={options.from} tx={options.tx} compilations={compilations} provider={provider} />
+          </Chakra.TabPanel>
+          <Chakra.TabPanel>
+            Debug panel
+          </Chakra.TabPanel>
+        </Chakra.TabPanels>
+      </Chakra.Tabs>
+    </Chakra.Box>
   );
 }
 
