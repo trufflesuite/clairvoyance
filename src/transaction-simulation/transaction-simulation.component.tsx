7f7fe4a8a921673dd998d7f7ff12f19723937bec
MicaiahReid <micaiahreid@gmail.com>
2022-05-14 00:10:00 -0400

    this is it

--- src/transaction-simulation/transaction-simulation.component.tsx
+++ src/transaction-simulation/transaction-simulation.component.tsx
@@ -6,12 +6,23 @@ import { Progress } from '../progress/progress.component';
 import * as Chakra from "@chakra-ui/react";
 import styles from "./transaction-simulation.module.css";
 import { Options } from '../types/types';
+import { BalanceSummary } from 'src/balance-summary/balance-summary.component';
 
-export function TransactionSimulation({ progress, receipt, decoder, callResult, options }: {progress: OpCode | null, receipt: any, decoder: ProjectDecoder | null, callResult: any, options: Options}) {
+type TransactionSimulationProps = {
+  progress: OpCode | null, 
+  receipt: any, 
+  decoder: ProjectDecoder | null, 
+  callResult: any, 
+  options: Options,
+  balances: any
+}
+export function TransactionSimulation({ progress, receipt, decoder, callResult, options, balances }: TransactionSimulationProps ) {
   return (<div>
     <Events decoder={decoder} events={receipt?.logs || null} />
     <Progress progress={progress} />
     <Chakra.Heading fontSize="larger" className={styles.header}>Receipt Details</Chakra.Heading>
     <ReceiptDetails receipt={receipt} callResult={callResult} options={options}/>
+    <Chakra.Heading fontSize="larger" className={styles.header}>Balance Summary</Chakra.Heading>
+    <BalanceSummary receipt={receipt} balances={balances} options={options}/>
   </div>)
 }
